09f0053d484af3f7c1d62d52bee67fb119ced33d
NinaPivneva <okovd@yandex.ru>
2022-12-25 23:33:08 +0300

    1 done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// **************************************Задача 34: ***********************************
+// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+// int size = ReadInt("Введите объем/размер массива: ");
+// int [] numbers = new int[size];
+
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+// int result = 0;
+
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] % 2 == 0)
+//     {
+//         result++;
+//     }
+// }
+// if (result % 10 == 1)
+// {
+//     Console.WriteLine($"В массиве {result} четное число");
+// }
+// if (result % 10 == 2 || result % 10 == 3 || result % 10 == 4)
+// {
+//     Console.WriteLine($"В массиве {result} четных числа");
+// }
+// else
+
+//     Console.WriteLine($"В массиве {result} четных чисел");
+
+// // Методы
+// void FillArrayRandomNumbers(int [] array) //Заполнение массива
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(99, 999);
+//     }
+// }
+
+// void PrintArray(int[] array) //Вывод массива на экран
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+
+// int ReadInt(string message)  //Функция ввода
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+
+
+// **************************************Задача 36: ***********************************
+//Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+
+
+
+// **************************************Задача 38: ***********************************
+//Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
