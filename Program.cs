f92455b7570b2e855406e2131d21ca76c78cc566
NinaPivneva <okovd@yandex.ru>
2022-12-25 23:48:13 +0300

    done

--- Program.cs
+++ Program.cs
@@ -32,7 +32,8 @@
 //     Console.WriteLine($"В массиве {result} четных чисел");
 
 // // Методы
-// void FillArrayRandomNumbers(int [] array) //Заполнение массива
+// //Заполнение массива
+// void FillArrayRandomNumbers(int [] array) 
 // {
 //     for (int i = 0; i < array.Length; i++)
 //     {
@@ -40,7 +41,8 @@
 //     }
 // }
 
-// void PrintArray(int[] array) //Вывод массива на экран
+// //Вывод массива на экран
+// void PrintArray(int[] array) 
 // {
 //     for (int i = 0; i < array.Length; i++)
 //     {
@@ -49,8 +51,8 @@
 //     Console.WriteLine();
 // }
 
-
-// int ReadInt(string message)  //Функция ввода
+// //Функция ввода
+// int ReadInt(string message)  
 // {
 //     Console.Write(message);
 //     return Convert.ToInt32(Console.ReadLine());
@@ -84,7 +86,9 @@
 // Console.WriteLine($"Сумма элементов нечетных позиций = {result}");
 
 // // Методы
-// void FillArrayRandomNumbers(int [] array) //Заполнение массива
+
+// //Заполнение массива
+// void FillArrayRandomNumbers(int [] array) 
 // {
 //     for (int i = 0; i < array.Length; i++)
 //     {
@@ -92,7 +96,8 @@
 //     }
 // }
 
-// void PrintArray(int[] array) //Вывод массива на экран
+// //Вывод массива на экран
+// void PrintArray(int[] array) 
 // {
 //     for (int i = 0; i < array.Length; i++)
 //     {
@@ -101,7 +106,8 @@
 //     Console.WriteLine();
 // }
 
-// int ReadInt(string message)  //Функция ввода
+// //Функция ввода
+// int ReadInt(string message)  
 // {
 //     Console.Write(message);
 //     return Convert.ToInt32(Console.ReadLine());
@@ -114,3 +120,56 @@
 //Найдите разницу между максимальным и минимальным элементов массива.
 
 // [3 7 22 2 78] -> 76
+
+// int size = ReadInt("Введите объем/размер массива: ");
+// int min = ReadInt("Введите минимальное число массива: ");
+// int max = ReadInt("Введите максимальное число массива: ");
+// int [] numbers = new int[size];
+
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+
+// int maxFillarray = numbers[0];
+// int minFillarray = numbers[0];
+
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] > maxFillarray)
+//     {
+//         maxFillarray = numbers[i];
+//     }
+//     if (numbers[i] < minFillarray)
+//     {
+//         minFillarray = numbers[i];
+//     }
+// }
+// Console.WriteLine($"Разница между максимальным и минимальным числом = {maxFillarray - minFillarray}");
+
+
+// // Методы
+
+// //Заполнение массива
+// void FillArrayRandomNumbers(int [] array) 
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(min, max);
+//     }
+// }
+
+// //Вывод массива на экран
+// void PrintArray(int[] array) 
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// //Функция ввода
+// int ReadInt(string message)  
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
